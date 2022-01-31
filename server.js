27d530900123f35d14a8871f1c199a04a099a98a
Anton <pentyugov@icloud.com>
2022-01-31 23:10:45 +0400

    heroku

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+//Install express server
 const express = require('express');
 const path = require('path');
+
 const app = express();
-app.use(express.static(__dirname + '/dist/zolloz-client'));
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+
-    '/dist/zolloz-client/index.html'));});
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/zolloz-client'));
+
+app.get('/*', (req, res) =>
+  res.sendFile('index.html', {root: 'dist/zolloz-client/'}),
+);
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
