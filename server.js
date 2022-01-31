4d54f34f63aa13f8bc7accacc57334976c27e621
Anton <pentyugov@icloud.com>
2022-01-31 23:50:16 +0400

    heroku

--- server.js
+++ server.js
@@ -1,17 +1,35 @@
+// //Install express server
+// const express = require('express');
+// const path = require('path');
+//
+// const app = express();
+//
+// // Serve only the static files form the dist directory
+// app.use(express.static('./dist/zolloz-client'));
+//
+// app.get('/*', (req, res) =>
+//   res.sendFile('index.html', {root: 'dist/zolloz-client/'}),
+// );
+//
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 4200, function(){
+//   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+// });
+
+
 //Install express server
 const express = require('express');
+
 const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/zolloz-client'));
+app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/zolloz-client/'}),
-);
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+'/dist/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 4200, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+app.listen(process.env.PORT || 8080);
