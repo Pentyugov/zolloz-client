34ec3079e15d0cd6a600de0968614beb671305f5
Anton <pentyugov@icloud.com>
2022-01-31 22:18:56 +0400

    Add screen guard

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,11 @@
 
 export const environment = {
   production: false,
-  API_URL : 'http://localhost:8080/api',
-  WS_URL: 'http://localhost:8080/ws'
+  // API_URL : 'http://localhost:8080/api',
+  // WS_URL: 'http://localhost:8080/ws',
+
+  API_URL : 'https://zolloz.herokuapp.com/api',
+  WS_URL: 'https://zolloz.herokuapp.com/ws'
 };
 
 /*
